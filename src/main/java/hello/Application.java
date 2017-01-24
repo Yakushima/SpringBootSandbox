349734daa5a068a1796929255027a4450c6905ba
Yakushima <michael.eugene.turner@gmail.com>
2017-01-24 12:29:56 +0000

    Modified for war file packaging and deployment as ROOT

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -20,5 +22,12 @@ public class Application {
             System.out.println(beanName);
         }
     }
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+    
+    private static Class<Application> applicationClass = Application.class;
+    
 }
